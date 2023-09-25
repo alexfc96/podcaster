ee8c3620258fd041628cf4f9266acb2a9dba91e2
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-09-25 20:00:52 +0200

    chore: installin react-router-dom. Configure main.tsx and making clicable the card with redirection to a specific podcast page

--- src/components/PodcastCard/PodcastCard.tsx
+++ src/components/PodcastCard/PodcastCard.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Card, CardContent, CardHeader, CardMedia, Typography } from '@mui/material';
 import { Podcast } from '../../types';
+import { Link } from 'react-router-dom';
 import './PodcastCard.css';
 
 interface Props {
@@ -7,37 +8,41 @@ interface Props {
 }
 
 const PodcastCard = ({ podcast }: Props) => {
+  const podcastId = podcast.id.attributes['im:id'];
+
   return (
-    <Card className="card">
-      <CardHeader
-        avatar={
-          <Avatar
-            alt="Podcast"
-            src={podcast['im:image'][0].label}
-            sx={{
-              width: 100,
-              height: 100,
-              margin: '0 auto',
-            }}
-          />
-        }
-      />
-      <div className="media-container">
-        <CardMedia
-          className="media"
-          image={podcast['im:image'][0].label}
-          title="Podcast"
+    <Link to={`/podcast/${podcastId}`} className="card-link">
+      <Card className="card">
+        <CardHeader
+          avatar={
+            <Avatar
+              alt="Podcast"
+              src={podcast['im:image'][0].label}
+              sx={{
+                width: 100,
+                height: 100,
+                margin: '0 auto',
+              }}
+            />
+          }
         />
-      </div>
-      <CardContent className="content">
-        <Typography variant="h5" component="div">
-          {podcast['im:name'].label}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Author: {podcast['im:artist'].label}
-        </Typography>
-      </CardContent>
-    </Card>
+        <div className="media-container">
+          <CardMedia
+            className="media"
+            image={podcast['im:image'][0].label}
+            title="Podcast"
+          />
+        </div>
+        <CardContent className="content">
+          <Typography variant="h5" component="div">
+            {podcast['im:name'].label}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Author: {podcast['im:artist'].label}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
