c760a0fb0c4a817c70d0e88c0252085308a7af2b
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-09-25 20:25:39 +0200

    feat: searching podcasts by title and applying some style to the input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import PodcastCard from './components/PodcastCard/PodcastCard'
 import { Podcast } from './types'
+import { Container, TextField } from '@mui/material'
 
 function App() {
   const [podcasts, setPodcasts] = useState<Podcast[]>([])
+  const [filterPodcast, setFilterPodcast] = useState<string>('')
+
+  const filteredPodcasts = useMemo(() => {
+    return filterPodcast != null && filterPodcast.length > 0
+    ? podcasts.filter(podcast => {
+        return podcast['im:name'].label.toLocaleLowerCase().includes(filterPodcast.toLocaleLowerCase())
+      })
+    : podcasts
+  }, [podcasts, filterPodcast]) 
 
   useEffect(() =>{
     fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
@@ -19,16 +29,23 @@ function App() {
   }, [])
 
   return (
-    <>
+    <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
       <Header />
+      <TextField
+        placeholder='Filter podcast'
+        onChange={(e) => {
+          setFilterPodcast(e.target.value);
+        }}
+        sx={{ marginBottom: 2 }} // Espacio entre el campo de búsqueda y las tarjetas
+      />
       <div className="card-container">
-        {podcasts &&
-          podcasts.map((podcast: Podcast) => (
+        {filteredPodcasts &&
+          filteredPodcasts.map((podcast: Podcast) => (
             <PodcastCard podcast={podcast} key={podcast.id.attributes['im:id']} />
-            ))
+          ))
         }
       </div>
-    </>
+    </Container>
   )
 }
 
