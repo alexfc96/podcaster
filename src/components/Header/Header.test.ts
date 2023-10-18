3b57a043faee0939b81eeaee82a3e8d5e7d5d932
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-10-18 12:00:46 +0200

    test: installing and configuring jest and testing library on a vite project

---
+++ src/components/Header/Header.test.ts
@@ -0,0 +1,13 @@
+// import "@testing-library/jest-dom/extend-expect";
+// import { render } from "@testing-library/react";
+// import Header from "./Header";
+
+test('demo', () => {
+    expect(true).toBe(true)
+})
+
+// test("renders content", () => {
+//     const component = render(<Header isLoading="false" />);
+
+//     component.getByText("Podcaster")
+// })
